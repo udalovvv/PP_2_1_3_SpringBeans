0167b7c4c616670590d123e5dfe277bcebce9f93
udalovvv <udalov.vitalii@gmail.com>
2022-08-23 01:28:59 +0500

    version 1.0

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,8 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+}
